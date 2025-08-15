759b3f0c5f057e2e9ab1446f6c46965f5e7e99a7
Rizky Putra <49565820+ednar28@users.noreply.github.com>
2025-08-15 16:05:36 +0700

    perbaiki error ts

--- rollup.config.js
+++ rollup.config.js
@@ -2,11 +2,13 @@ import typescript from '@rollup/plugin-typescript';
 import { nodeResolve } from '@rollup/plugin-node-resolve';
 import commonjs from '@rollup/plugin-commonjs';
 import terser from '@rollup/plugin-terser';
+import dts from 'rollup-plugin-dts';
 import pkg from './package.json' with { type: 'json' };
 
 const isProduction = process.env.NODE_ENV === 'production';
 
-export default {
+export default [{
+  // JavaScript build
   input: 'src/index.ts',
   external: ['vue', 'yup'],
   output: [
@@ -30,10 +32,18 @@ export default {
     commonjs(),
     typescript({
       tsconfig: './tsconfig.json',
-      declaration: true,
-      declarationDir: './dist/types',
+      declaration: false,
       rootDir: './src'
     }),
     isProduction && terser()
   ].filter(Boolean)
-};
+}, {
+  // TypeScript declarations build
+  input: 'src/index.ts',
+  external: ['vue', 'yup'],
+  output: {
+    file: pkg.types,
+    format: 'es'
+  },
+  plugins: [dts()]
+}];
